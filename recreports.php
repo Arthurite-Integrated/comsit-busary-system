a1083d4a517998218e2df1410e560fc740943f86
halsofttech <codesplashers.musa@gmail.com>
2026-09-15 06:22:47 +0100

    Add files via upload

--- recreports.php
+++ recreports.php
@@ -744,12 +744,12 @@ if(isset($_POST['btn_outflow']) and isset($_SESSION['r_m']) and $_SESSION['r_m']
 	//===============> BANK LEFT-OVER 
 	$sqlb="SELECT * FROM recon_banktb WHERE rmonth='".$_SESSION['r_m']."' and ryear='".$_SESSION['r_y']."' and Ref != '2' AND paytype='Debit'";
 	$qryb= mysqli_query($con, $sqlb); $sn=0; $lefttotal=0; $nm= mysqli_num_rows($qryb);
-	echo $brl1x .= "<TABLE width='90%' border='1' rules='rows' align='center'>
-	<tr><td><strong>SN</strong></td><td><strong>TRANS. REF.</strong></td><td><strong>DESCRIPTION</strong></td><td><strong>AMOUNT</strong></td><td>PERIOD</td></tr>";exit;
+	$brl1x .= "<TABLE width='90%' border='1' rules='rows' align='center'>
+	<tr><td><strong>SN</strong></td><td><strong>TRANS. REF.</strong></td><td><strong>DESCRIPTION</strong></td><td><strong>AMOUNT</strong></td><td>PERIOD</td></tr>";
 	while($r= mysqli_fetch_array($qryb, 3 )){
 		++$snx; 
-		$lefttotal += abs($r['amount']);
-		echo $brl1x .= "<tr><td>{$snx}</td><td>".$r['credit_reference']."</td><td style='font-size:10px;'>{$r[paymentid]}</td><td>".number_format(abs($r['amount']), 2)."</td><td>{$r['rmonth']}/{$r['ryear']}</td></tr>";
+		$lefttotal += ($r['amount']);
+		echo $brl1x .= "<tr><td>{$snx}</td><td>".$r['credit_reference']."</td><td style='font-size:10px;'>{$r[paymentid]}</td><td>".number_format(($r['amount']), 2)."</td><td>{$r['rmonth']}/{$r['ryear']}</td></tr>";
 	}
 	exit;
 	$lefttotal = number_format($lefttotal, 2);
