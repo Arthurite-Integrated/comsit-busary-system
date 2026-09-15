f76a6dc00f84a2db25ab047bb04d257628128800
halsofttech <codesplashers.musa@gmail.com>
2026-09-15 06:25:24 +0100

    Add files via upload

--- recreports.php
+++ recreports.php
@@ -748,8 +748,8 @@ if(isset($_POST['btn_outflow']) and isset($_SESSION['r_m']) and $_SESSION['r_m']
 	<tr><td><strong>SN</strong></td><td><strong>TRANS. REF.</strong></td><td><strong>DESCRIPTION</strong></td><td><strong>AMOUNT</strong></td><td>PERIOD</td></tr>";
 	while($r= mysqli_fetch_array($qryb, 3 )){
 		++$snx; 
-		$lefttotal += ($r['amount']);
-		echo $brl1x .= "<tr><td>{$snx}</td><td>".$r['credit_reference']."</td><td style='font-size:10px;'>{$r[paymentid]}</td><td>".number_format(($r['amount']), 2)."</td><td>{$r['rmonth']}/{$r['ryear']}</td></tr>";
+		$lefttotal += abs($r['amount']);
+		echo $brl1x2 .= "<tr><td>{$snx}</td><td>".$r['credit_reference']."</td><td style='font-size:10px;'>{$r[paymentid]}</td><td>".number_format(abs($r['amount']), 2)."</td><td>{$r['rmonth']}/{$r['ryear']}</td></tr>";
 	}
 	exit;
 	$lefttotal = number_format($lefttotal, 2);
