7657edfdb063f7278f22ad1fbb496a68ee4c0eff
halsofttech <codesplashers.musa@gmail.com>
2026-09-16 10:55:06 +0100

    Add files via upload

--- mail copy.php
+++ mail copy.php
@@ -681,7 +681,7 @@ if(cv=='out_query') // out query
                     //if(strtolower($r_vals) == "administrator" || strtolower($r_vals) == "super admin")
                     //$sql="SELECT mm.memo_id, m.memo_from, m.description, m.amount, m.memo_status, m.datein, mm.read_status, mm.dept_unit, m.entry_time, m.address_unit from memo_movementtb mm inner join memotb m on mm.memo_id=m.memo_id where mm.memo_status='IN' and  mm.read_status='Unread' order by mm.id desc";
                     //else
-                    echo $sql="SELECT `mm`.`memo_id`, `m`.`memo_from`, `m`.`description`, `m`.`amount`, `m`.`memo_status`, `m`.`datein`, `mm`.`read_status`, `mm`.`memo_to`, `m`.`entry_time`, `m`.`address_unit`, mm.id FROM `memo_movementtb` `mm` INNER JOIN `memotb` `m` ON `mm`.`memo_id`=`m`.`memo_id` WHERE `mm`.`memo_status`='IN' AND  `mm`.`read_status`='Unread' AND (`mm`.`memo_from`='".@$_SESSION['userunit']."' OR m.`entry_by`='".$_SESSION['login_id']."') ORDER BY mm.id DESC LIMIT 500";
+                    $sql="SELECT `mm`.`memo_id`, `m`.`memo_from`, `m`.`description`, `m`.`amount`, `m`.`memo_status`, `m`.`datein`, `mm`.`read_status`, `mm`.`deptunit_to`, `m`.`entry_time`, `m`.`address_unit`, mm.id FROM `memo_movementtb` `mm` INNER JOIN `memotb` `m` ON `mm`.`memo_id`=`m`.`memo_id` WHERE `mm`.`memo_status`='IN' AND  `mm`.`read_status`='Unread' AND (`mm`.`dept_unit`='".@$_SESSION['userunit']."' OR m.`entry_by`='".$_SESSION['login_id']."') ORDER BY mm.id DESC LIMIT 500";
                     //`m`.`datein`, `m`.`entry_time`
                     $rq=mysqli_query($con, $sql);
                     //$r=mysqli_fetch_array($rq, 3);
@@ -700,7 +700,7 @@ if(cv=='out_query') // out query
                             </div>
                         </td>
                         <td>
-                            <?=$bursary->get_any_value("surname", 'stafftb', "fileno", $row['memo_to']);?> <?=$bursary->get_any_value("first_name", 'stafftb', "fileno", $row['memo_to']);?> <?=$bursary->get_any_value("other_name", 'stafftb', "fileno", $row['memo_to']);?>
+                            <?=$bursary->get_any_value("surname", 'stafftb', "fileno", $row['deptunit_to']);?> <?=$bursary->get_any_value("first_name", 'stafftb', "fileno", $row['deptunit_to']);?> <?=$bursary->get_any_value("other_name", 'stafftb', "fileno", $row['deptunit_to']);?>
                         </td>
                         <td><?=$row['description']?></td>
                         <td>
