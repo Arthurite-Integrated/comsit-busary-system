dbc0b91f4f74aa38cba884b30a553807ac68230b
halsofttech <codesplashers.musa@gmail.com>
2026-09-26 12:35:09 +0100

    Add files via upload

--- mail_incoming.php
+++ mail_incoming.php
@@ -468,7 +468,7 @@ include "function.php";?>
                                 <?php
                                 $qmail=mysqli_query($con, $sq);
                                 while($row=mysqli_fetch_array($qmail, 3)){
-                                    $row['address_unit']=='' ? $dept_from='Central' : $dept_to=get_unit_name('', $row['address_unit']);
+                                    !is_numeric($row['address_unit']) ? $dept_from=$row['address_unit'] : $dept_from=get_unit_name('', $row['address_unit']);
                                     $row['dept_unit']=='' ? $dept_to='Central' : $dept_to=get_unit_name('', $row['dept_unit']);
                                     ?>
                                     <tr  style="color:#900">
