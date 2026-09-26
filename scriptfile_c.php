9e7f802fd943b787549f2495090eac40e1fbd4d5
halsofttech <codesplashers.musa@gmail.com>
2026-09-26 11:38:04 +0100

    Add files via upload

--- scriptfile_c.php
+++ scriptfile_c.php
@@ -1811,7 +1811,8 @@ if($id=='allocaterole_section')
      $login_id=@$_SESSION['login_id'];
      if($action=='save')
      {
-          @mysqli_query($con, "INSERT INTO users_roletb set fileno='{$j->fileno}', role='{$j->role}',dept_acctcode='{$j->dept_acctcode}',entry_date=CURDATE(),entry_time=CURTIME(),entry_by='{$login_id}'");
+          echo $sql="INSERT INTO users_roletb set fileno='{$j->fileno}', role='{$j->role}', dept_acctcode='{$j->dept_acctcode}', entry_date=CURDATE(), entry_time=CURTIME(), entry_by='{$login_id}'"; exit;
+          @mysqli_query($con, $sql);
           echo "<script>alert('Record saved successfully');</script>";
 
           $sql="SELECT * from users_roletb where fileno='{$j->fileno}'";
